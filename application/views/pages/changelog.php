31b92185b50603df558f286bf7e4f420434d9034
valentinsocha <valentin.socha@gmail.com>
2020-04-15 15:05:00 +0200

    Update public changelog

--- application/views/pages/changelog.php
+++ application/views/pages/changelog.php
@@ -4,6 +4,10 @@
             <a href="<?php echo base_url(); ?>">Retour à l'accueil</a>
         </div>
         <div class="version mb-6">
+            <h2 class="mb-4">Version 0.0.2 - 15/04/2020</h2>
+            <p>Mise à jour de l'avertissement sur les pages outils</p>
+        </div>
+        <div class="version mb-6">
             <h2 class="mb-4">Version 0.0.1 - 13/04/2020</h2>
             <p>Version initiale du projet</p>
         </div>
